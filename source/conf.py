089e4b7622cfc6fbd5a07624b00ee283e792a070
Chris Nasr <ouroboroscode@gmail.com>
2021-04-21 12:24:14 -0400

    - 0.8.0

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, OuroborosCoding'
 author = 'OuroborosCoding'
 
 # The full version, including alpha/beta/rc tags
-release = '0.7.0'
+release = '0.8.0'
 
 
 # -- General configuration ---------------------------------------------------
